3890cb077c6cca096ba9e6f0577eaf2ebb380389
Vitor <vitorhugosantos144@gmail.com>
2022-05-02 22:41:35 +0100

    cmake added

---
+++ cmake-build-debug/CMakeFiles/cough_cough.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/from_N_processes_to_file/from_N_processes_to_file.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/from_N_processes_to_file/from_N_processes_to_file.c.o"
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/from_N_processes_to_parent_to_M_processes/from_N_processes_to_parent_to_M_processes.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/from_N_processes_to_parent_to_M_processes/from_N_processes_to_parent_to_M_processes.c.o"
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/from_N_processes_to_parent_to_file/from_N_processes_to_parent_to_file.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/from_N_processes_to_parent_to_file/from_N_processes_to_parent_to_file.c.o"
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/from_socket_to_file/from_socket_to_file.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/from_socket_to_file/from_socket_to_file.c.o"
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/libs/io/io.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/libs/io/io.c.o"
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/libs/occupation/occupation.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/libs/occupation/occupation.c.o"
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/libs/parser/parser.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/libs/parser/parser.c.o"
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/libs/socket/socket.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/libs/socket/socket.c.o"
+  "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/main.c" "/Users/VitorHugo/dev/c/CLionProjects/cough_cough/cmake-build-debug/CMakeFiles/cough_cough.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../data"
+  "../libs"
+  "../occupation"
+  "../parser"
+  "../io"
+  "../socket"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cough_cough.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cough_cough.dir/from_N_processes_to_file/from_N_processes_to_file.c.o"
+  "CMakeFiles/cough_cough.dir/from_N_processes_to_parent_to_M_processes/from_N_processes_to_parent_to_M_processes.c.o"
+  "CMakeFiles/cough_cough.dir/from_N_processes_to_parent_to_file/from_N_processes_to_parent_to_file.c.o"
+  "CMakeFiles/cough_cough.dir/from_socket_to_file/from_socket_to_file.c.o"
+  "CMakeFiles/cough_cough.dir/libs/io/io.c.o"
+  "CMakeFiles/cough_cough.dir/libs/occupation/occupation.c.o"
+  "CMakeFiles/cough_cough.dir/libs/parser/parser.c.o"
+  "CMakeFiles/cough_cough.dir/libs/socket/socket.c.o"
+  "CMakeFiles/cough_cough.dir/main.c.o"
+  "cough_cough"
+  "cough_cough.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cough_cough.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
