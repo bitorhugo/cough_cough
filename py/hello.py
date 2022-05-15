3dd1c82dc44670116a951c49736ebb71a59410ce
Vitor <vitorhugosantos144@gmail.com>
2022-05-16 00:26:48 +0100

    2B done

--- py/hello.py
+++ py/hello.py
@@ -1,8 +1,10 @@
 import sys
 
+
 def main():
     for line in sys.stdin:
         print(line)
+
 
 if __name__ == '__main__':
     main()
--- py/plot.py
+++ py/plot.py
@@ -3,19 +3,20 @@ import pandas as pd
 import numpy as np
 from plotnine import *
 
-#time:count
+
+# time:count
 
 def main():
-    print('hello')
 
     fn = sys.argv[1]
 
     df = pd.read_csv(sys.stdin)
 
-    #print(df) # for debug
+    # print(df) # for debug
 
-    p = ggplot(df) + aes(x="time", y="count") + geom_area(position = "identity") + geom_line() + geom_point()
-    p.save(filename = fn + '.png', height=12, width=12, units = 'cm', dpi=300)
+    p = ggplot(df) + aes(x="time", y="count") + geom_area(position="identity") + geom_line() + geom_point()
+    p.save(filename=fn + '.png', height=12, width=12, units='cm', dpi=300)
+
 
 if __name__ == '__main__':
     main()
